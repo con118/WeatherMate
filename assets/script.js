310b31195145e276ee655beca5e1b057e887532b
con118 <42159623+con118@users.noreply.github.com>
2023-04-02 11:26:20 +1000

    Update script.js

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,19 @@
+const API_KEY = 'your_api_key_here';
+const latitude = '37.7749'; // example latitude
+const longitude = '-122.4194'; // example longitude
+const api_url = `https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=${API_KEY}`;
 
+fetch(api_url)
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    return response.json();
+  })
+  .then(data => {
+    // Use the data retrieved from the API here
+    console.log(data);
+  })
+  .catch(error => {
+    console.error('There was a problem with the fetch request:', error);
+  });
